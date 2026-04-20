a624c1ccfed217a647ef5cee84ca11049e2ce4a8
Serina <sirenalovesyou5@gmail.com>
2026-04-20 08:13:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import random
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 pygame.init()
@@ -14,18 +15,47 @@ pygame.display.set_caption('Orbital Escapades')
 clock = pygame.time.Clock()
 
 background = pygame.image.load("stars.jpg")
+pointer = pygame.image.load("pointer.png")
+pointer = pygame.transform.scale(pointer, (30, 45))
 
+planet = pygame.image.load("planet.png")
+planet = pygame.transform.scale(planet, (30, 40))
 
+x_pos = []
+y_pos = []
+sx = []
+sy = []
+
+pygame.mouse.set_visible(False)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1: # 1 is Left Click
+                print(f"Left click at {event.pos}")
+                x_pos.append(event.pos[0])
+                y_pos.append(event.pos[1])
+                sx.append(random.random() * 10 - 5)
+                sy.append(random.random() * 10 - 5)
+            elif event.button == 3: # 3 is Right Click
+                print("Right click!")
     
 
     screen.fill((255, 255, 255))
     screen.blit(background, (0, 0))
+    screen.blit(pointer, (pygame.mouse.get_pos()[0] - 0, pygame.mouse.get_pos()[1] - 10))
+
+    for i in range(sx.__len__()):
+        screen.blit(planet, (x_pos[i], y_pos[i]))
+        sx[i] += (pygame.mouse.get_pos()[0] - x_pos[i]) / 3
+        sy[i] += (pygame.mouse.get_pos()[0] - y_pos[i]) / 3
+        x_pos[i] += sx[i]
+        y_pos[i] += sy[i]
+
     pygame.display.flip()
     clock.tick(60)
 
